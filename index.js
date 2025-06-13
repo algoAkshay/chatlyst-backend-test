e821d18db53bafbf5fc539b36ba39a7a7e8304d2
akshay2023007 <143496049+akshay2023007@users.noreply.github.com>
2025-06-14 02:10:21 +0530

    updated index.js

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 import dotenv from "dotenv";
 dotenv.config();
 import express from "express"
-import dotenv from "dotenv"
 import connectDb from "./config/db.js"
 import authRouter from "./routes/auth.routes.js"
 import cookieParser from "cookie-parser"
